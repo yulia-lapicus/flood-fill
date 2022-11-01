5ec936406f5e1fa54080328a25865454dc956ad9
ROIM1998 <zhaobw1998@sina.com>
2022-10-31 17:56:35 -0700

    Add docstring and type assertions for defined functions

--- flood-fill.py
+++ flood-fill.py
@@ -13,7 +13,14 @@ board = [
 ]
 
 
-def get_valid_neighbour(board, x, y, old='.'):
+def get_valid_neighbour(board, x, y, old='.') -> List[List[int]]:
+    """Returns the neighbours of the given point that can be filled
+    Args:
+        board (List[str]) given board to check
+        x (int): Index on the vertical axis of the board
+        y (int): Index on the horizontal axis of the board
+        old (str): value to be replaced (or can be replaced)
+    """
     return [v for v in [(x+1, y), (x-1, y), (x, y+1), (x, y-1)] if 0 <= v[0] < len(board) and 0 <= v[1] < len(board[v[0]]) and board[v[0]][v[1]] == old]
 
 
@@ -21,11 +28,11 @@ def flood_fill(input_board: List[str], old: str, new: str, x: int, y: int) -> Li
     """Returns board with old values replaced with new values
     through flood filling starting from the coordinates x, y
     Args:
-        input_board (List[str])
+        input_board (List[str]) given board to fill
         old (str): Value to be replaced
-        new (str): Value to
-        x (int): _description_
-        y (int): _description_
+        new (str): Value for replacing
+        x (int): index on the vertical axis
+        y (int): index on the horizontal axis
     Returns:
         List[str]: Modified board
     """

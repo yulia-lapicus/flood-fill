30eacb739d8b19d50b8003f29333db99ee57518d
Yulia Lápicus <julia@lapicus.me>
2022-11-08 15:19:38 -0800

    Made changes for Baja

--- flood-fill.py
+++ flood-fill.py
@@ -1,6 +1,5 @@
 from typing import List
-from queue import Queue
-#input
+
 board = [
     "......................",
     "......##########......",
@@ -12,43 +11,42 @@ board = [
     "....##############....",
 ]
 
-#get neighbour
-def get_valid_neighbour(board, x, y, old='.') -> List[List[int]]:
-    """Returns the neighbours of the given point that can be filled
-    Args:
-        board (List[str]) given board to check
-        x (int): Index on the vertical axis of the board
-        y (int): Index on the horizontal axis of the board
-        old (str): value to be replaced (or can be replaced)
-    """
-    return [v for v in [(x+1, y), (x-1, y), (x, y+1), (x, y-1)] if 0 <= v[0] < len(board) and 0 <= v[1] < len(board[v[0]]) and board[v[0]][v[1]] == old]
-
-
 def flood_fill(input_board: List[str], old: str, new: str, x: int, y: int) -> List[str]:
     """Returns board with old values replaced with new values
     through flood filling starting from the coordinates x, y
     Args:
-        input_board (List[str]) given board to fill
+        input_board (List[str])
         old (str): Value to be replaced
-        new (str): Value for replacing
-        x (int): index on the vertical axis
-        y (int): index on the horizontal axis
+        new (str): Value that replaces the old
+        x (int): X-coordinate of the flood start point
+        y (int): Y-coordinate of the flood start point
     Returns:
         List[str]: Modified board
     """
-    q = Queue()
-    if input_board[x][y] == old:
-        board_str_list = list(input_board[x])
-        board_str_list[y] = new
-        input_board[x] = ''.join(board_str_list)    
-        valid_neighbours = get_valid_neighbour(input_board, x, y)            
-        for neighbor_x, neighbor_y in valid_neighbours:
-            _ = flood_fill(input_board, old, new, neighbor_x, neighbor_y)
+    
+    # Implement your code here.
+    if input_board[x][y] == "#" or x < 0 or y < 0 or x > len(input_board) or y > len(input_board[x]) or input_board[x][y] == new:        return
+
+    else: 
+        input_board[x] = input_board[x][:y] + new + input_board[x][y+1:]
+        flood_fill(input_board=input_board, old=old, new=new, x=x-1, y=y)
+        flood_fill(input_board=input_board, old=old, new=new, x=x, y=y-1)
+        flood_fill(input_board=input_board, old=old, new=new, x=x+1, y=y)
+        flood_fill(input_board=input_board, old=old, new=new, x=x, y=y+1)
+    
     return input_board
 
-    
-if __name__ == "__main__":
+modified_board = flood_fill(input_board=board, old=".", new="~", x=5, y=12)
 
-    ans = flood_fill(input_board=board, old=".", new="~", x=5, y=12)
-    for a in ans:
-        print(a)
+for a in modified_board:
+    print(a)
+
+# Expected output:
+# ......................
+# ......##########......
+# ......#~~~~~~~~#......
+# ......#~~~~~~~~#......
+# ......#~~~~~~~~#####..
+# ....###~~~~~~~~~~~~#..
+# ....#~~~~~~~~~~~~###..
+# ....##############....
